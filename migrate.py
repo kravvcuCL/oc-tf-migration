7db45a6749311412b08768988a66ac7d4f8b528b
Jarek Łukow <jaroslaw.lukow@codilime.com>
2018-12-31 12:14:34 +0100

    Rewrite with Repo class instead of tuple
    Add cli args

--- migrate.py
+++ migrate.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import os
 import subprocess
@@ -5,10 +6,12 @@ import yaml
 import pathlib
 import pygit2
 import jinja2
+import shutil
 from jinja2 import Template
 
 
 cfg = {}
+extra_env = {'GIT_SSH_COMMAND': 'ssh -i {}/id_rsa -o IdentitiesOnly=yes'.format(os.getcwd())}
 
 
 def read_yaml(path):
@@ -36,9 +39,11 @@ def read_config():
     cfg.update(new_cfg)
 
 
-def exec(cmd, cwd='.'):
-    print('executing:', cmd, 'in:', cwd)
-    subprocess.check_call(cmd, cwd=cwd)
+def exec(cmd, cwd='.', extra_env=extra_env):
+    environment = os.environ.copy()
+    environment.update(extra_env)
+    print('executing:', cmd, 'in:', cwd, 'extra env:', extra_env)
+    subprocess.check_call(cmd, cwd=cwd, env=environment)
 
 
 def get_active_branches():
@@ -53,51 +58,85 @@ def get_active_branches():
     return list(branches)
 
 
-def extract_reponames():
+class Repo():
+
+    def __init__(self, old_org, old_name, new_org, new_name, old_remote='github.com'):
+        self.old_org = old_org
+        self.old_name = old_name
+        self.new_org = new_org
+        self.new_name = new_name
+        self.old_remote = old_remote
+
+    def new_full_name(self):
+        return '{}/{}'.format(self.new_org, self.new_name)
+
+    def old_full_name(self):
+        return '{}/{}'.format(self.old_org, self.old_name)
+
+    def __str__(self):
+        return 'old: {}, new: {}'.format(self.old_full_name(), self.new_full_name())
+
+    def __repr__(self):
+        return 'Repo<' + self.__str__() + '>'
+
+
+def extract_reponames(suffix=''):
     fname = cfg['repos_csv_file_path']
     repos = []
+    obj = []
     github_repos = []
     with open(fname) as csvfile:
         reader = csv.reader(csvfile)
         next(reader)
         next(reader)
         for row in reader:
-            old = '{}/{}'.format(row[0], row[1])
-            new = '{}/{}'.format(row[2], row[3])
+            old_org = row[0]
+            new_org = row[2] + suffix
+            repo = Repo(old_org, row[1], new_org, row[3])
+            old = '{}/{}'.format(old_org, row[1])
+            new = '{}/{}'.format(new_org, row[3])
             if row[4].lower() == 'no':
                 github_repos.append((old, new))
             else:
                 repos.append((old, new))
-    return repos, github_repos
+            obj.append(repo)
+    return repos, github_repos, obj
 
 
-def clone_repos(repos):
-    gitdir = './git'
-    old = cfg['old_hostname']
-    new = cfg['new_hostname']
-    old_url = 'https://' + old
-    new_url = 'https://' + new
-    old_dir = gitdir + '/' + old
-    new_dir = gitdir + '/' + new
-    os.makedirs(old_dir, exist_ok=True)
+def filter_repos(repos, old_short_names):
+    return [r for r in repos if r.old_name in old_short_names]
+
+
+def clone_repos(repos, remove=False):
     for repo in repos:
-        repo_path = get_old_repo_path(repo[0])
+        old_dir = cfg['gitdir'] + '/' + repo.old_remote
+        os.makedirs(old_dir, exist_ok=True)
+        old_url = 'https://' + repo.old_remote
+        repo_path = get_old_repo_path(repo)
         repo_path = pathlib.Path(repo_path)
         os.makedirs(str(repo_path.parent), exist_ok=True)
+        if remove:
+            print('Removing repo dir:', str(repo_path))
+            shutil.rmtree(str(repo_path))
         try:
             exec(['git', '-C', str(repo_path), 'fetch', '--all'])
             exec(['git', '-C', str(repo_path), 'pull'])
         except subprocess.CalledProcessError:
-            exec(['git', 'clone', old_url + '/' + repo[0], repo[0]], old_dir)
+            exec(['git', 'clone', old_url + '/' + repo.old_full_name(), repo.old_full_name()], old_dir)
+
+
+def get_old_repo_path_(repo):
+    gitdir = pathlib.Path(cfg['gitdir'])
+    old = cfg['old_hostname']
+    old_dir = gitdir / old
+    repo_path = old_dir / repo
+    return str(repo_path)
 
 
 def get_old_repo_path(repo):
     gitdir = pathlib.Path(cfg['gitdir'])
-    old = cfg['old_hostname']
-    new = cfg['new_hostname']
-    old_dir = gitdir / old
-    new_dir = gitdir / new
-    repo_path = old_dir / repo
+    old_dir = gitdir / repo.old_remote
+    repo_path = old_dir / repo.old_org / repo.old_name
     return str(repo_path)
 
 
@@ -122,12 +161,12 @@ def is_branch_migrated(repo, branch):
 
 
 def squash_all(repos, branches):
-    for old_repo, new_repo in repos:
+    for repo in repos:
         for branch in branches:
-            if is_branch_migrated(old_repo, branch):
-                squash(old_repo, branch)
+            if is_branch_migrated(repo, branch):
+                squash(repo, branch)
             else:
-                print('branch', branch, 'not in repo', old_repo)
+                print('branch', branch, 'not in repo', repo.old_full_name())
 
 
 def sed_dir(pattern_from, pattern_to, path):
@@ -137,7 +176,7 @@ def sed_dir(pattern_from, pattern_to, path):
 
 
 def patch(repo, branch, repos):
-    repo_path = get_old_repo_path(repo)
+    repo_path = get_old_repo_path_(repo)
     patch_path = os.getcwd() + '/patches/' + repo.split('/')[-1] + '.patch'
     print(patch_path)
     if os.path.exists(patch_path):
@@ -145,29 +184,61 @@ def patch(repo, branch, repos):
         exec(['git', 'apply', patch_path], repo_path)
     for old, new in repos:
         sed_dir(old, new, repo_path)
-    pass
+    cmd = ['git', 'add', '-A']
+    exec(cmd, cwd=repo_path)
+    cmd = ['git', 'commit', '-m', '.']
+    exec(cmd, cwd=repo_path)
 
 
-def push():
-    pass
+def push(repo, branches, dry_run=True):
+    remote_url_base = 'ssh://git@github.com'
+    remote_url = '{}/{}/{}'.format(remote_url_base, repo.new_org, repo.new_name)
+    path = get_old_repo_path(repo)
+    cmd = ['git', 'remote', 'add', 'new', remote_url]
+    print(cmd)
+    if not dry_run:
+        try:
+            exec(cmd, cwd=path)
+        except subprocess.CalledProcessError:
+            print('Remote already exists?')
+    for branch in branches:
+        if is_branch_migrated(repo, branch):
+            cmd = ['git', 'push', 'new', branch]
+            print(cmd)
+            if not dry_run:
+                exec(cmd, cwd=path)
+
+
+def push_all(repos, branches, dry_run=True):
+    for repo in repos:
+        push(repo, branches, dry_run)
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dry-run", action="store_true")
+    parser.add_argument("--full-reclone", action="store_true")
+    parser.add_argument("--single-repo", type=str)
+    args = parser.parse_args()
+    dry_run = args.dry_run
     read_config()
-    repos, github_repos = extract_reponames()
-    print(repos)
-    print(github_repos)
+    repos, github_repos, obj = extract_reponames(suffix='-preview')
+    if args.single_repo:
+        obj = filter_repos(obj, [args.single_repo])
+        print('Repos after filtering:', obj)
+    #print(repos)
+    #print(github_repos)
     active_branches = get_active_branches()
-    print(active_branches)
+    print('Active branches:', active_branches)
     if cfg['clone']:
-        clone_repos(repos)
-    clone_repos([('Juniper/contrail-project-config', '')])
-    #squash_all(repos, active_branches)
+        clone_repos(obj, remove=args.full_reclone)
+    squash_all(obj, active_branches)
     repos_fqdn = [(cfg['old_hostname'] + '/' + r[0], cfg['new_hostname'] + '/' + r[1]) for r in repos]
     github_repos_fqdn = [('github.com/' + r[0], cfg['new_hostname'] + '/' + r[1]) for r in github_repos]
     sr = sorted(repos + github_repos + repos_fqdn + github_repos_fqdn, key=lambda x: len(x[0]), reverse=True)
-    print(sr)
-    patch('Juniper/contrail-project-config', 'master', sr)
+    #print(sr)
+    #patch('Juniper/contrail-project-config', 'master', sr)
+    push_all(obj, active_branches, dry_run=dry_run)
 
 
 if __name__ == '__main__':
